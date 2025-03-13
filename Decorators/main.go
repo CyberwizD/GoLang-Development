7df22643882b153916d4f8775619af1f138199c4
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-03-13 03:29:24 +0100

    add: implement HTTP server with authorization header check and routing

--- Decorators/main.go
+++ Decorators/main.go
@@ -9,6 +9,8 @@ and append or prepend your own custom functionality on top.
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -31,6 +33,45 @@ func init() {
 	fmt.Println("Decorated function.")
 }
 
+func Page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+}
+
+func authPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Authentication Page")
+}
+
+func isAuthorizedHeaderTrue(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Checking if Authorized Header is set.")
+
+		AuthorizedHeader := r.Header.Get("Authorized")
+
+		if AuthorizedHeader == "true" { // Correct Logic
+			if value, ok := r.Header["Authorized"]; ok {
+				fmt.Println(value)
+
+				if value[0] == "true" {
+					fmt.Println("Authorized Header Set")
+				}
+
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Println("Not Authorized!")
+			fmt.Fprintf(w, "Not Authorized!")
+		}
+	})
+}
+
+func handleRequests() {
+	http.Handle("/", isAuthorizedHeaderTrue(Page))
+	http.Handle("/auth", isAuthorizedHeaderTrue(authPage))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	fmt.Println("Hello ")
+	fmt.Println("Decorating HTTP Web Server")
+
+	handleRequests()
 }
