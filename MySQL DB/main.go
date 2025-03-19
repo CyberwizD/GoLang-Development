bc858e2d07ee9c234d39a8a34ba6f8510adc331b
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-03-20 00:50:40 +0100

    add: implement MySQL database connection and insert functionality

--- MySQL DB/main.go
+++ MySQL DB/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("MySQL DB")
+
+	// Database connection.``
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	// Perform a db.Query insert
+	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+
+	// if there is an error inserting...
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+}
