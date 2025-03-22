370311cf349d4d7b1b849e7bd105935eb80175b4
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-03-22 15:07:49 +0100

    add: implement SELECT functionality and define Tag struct

--- MySQL DB/main.go
+++ MySQL DB/main.go
@@ -7,6 +7,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Tag struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 	fmt.Println("MySQL DB")
 
@@ -19,7 +24,7 @@ func main() {
 
 	defer db.Close()
 
-	// Perform a db.Query insert
+	// Perform a db.Query INSERT
 	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
 
 	// if there is an error inserting...
@@ -28,4 +33,25 @@ func main() {
 	}
 
 	defer insert.Close()
+
+	// Perform a db.Query SELECT
+	results, err := db.Query("SELECT id, name FROM test")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var tag Tag
+
+		// For each row, scan the result into our tag composite object
+		err = results.Scan(&tag.ID, &tag.Name)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Print out the tag's ID and Name attribute
+		fmt.Println(tag.ID, tag.Name)
+	}
 }
