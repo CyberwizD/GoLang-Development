bd8778f536ff847d6c4b4bdde06ae1f1970d92f7
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-03-25 10:38:25 +0100

    add: integrate GORM for MySQL database operations and implement user management endpoints

--- Go-ORM/go.mod
+++ Go-ORM/go.mod
@@ -3,6 +3,7 @@ module github.com/Cyberwizd/GoLang-Development
 go 1.24.0
 
 require (
+	github.com/go-orm/gorm v0.0.0-20161201081620-eb06255b667d // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 )
--- Go-ORM/main.go
+++ Go-ORM/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/go-orm/gorm"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 )
 
 // User Struct
@@ -18,18 +20,91 @@ type User struct {
 
 func allUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "All Users Endpoint Hit")
+
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+
+	defer db.Close()
+
+	var users []User
+
+	db.Find(&users)
+	fmt.Println("{}", users)
+
+	json.NewEncoder(w).Encode(users)
 }
 
 func newUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "New User Endpoint Hit")
+
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	db.Create(&User{Name: name, Email: email})
+
+	fmt.Fprintf(w, "New User Successfully Created")
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Delete User Endpoint Hit")
+
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	var user User
+
+	db.Where("name = ?", name).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "User Successfully Deleted")
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Update User Endpoint Hit")
+
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	var user User
+
+	db.Where("name = ?", name).Find(&user)
+	user.Email = email
+	db.Save(&user)
+
+	fmt.Fprintf(w, "User Successfully Updated")
 }
 
 func handleRequests() {
@@ -43,8 +118,24 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
+func initialMigration() {
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
+}
+
 func main() {
 	fmt.Println("Go ORM")
+
+	// Perform a migration
+	initialMigration()
 
 	// Handle Subsequent requests
 	handleRequests()
